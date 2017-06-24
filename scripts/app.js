5ab511217ec23b42211bfbceac98dd986e25d6f9
vaccarov <victo.vaccaro@gmail.com>
2017-06-24 20:50:24 +0200

    fix script error and change item design

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  var app = {
+    isLoading: true,
+    spinner: document.querySelector('.loader'),
+    container: document.querySelector('.main')
+  };
+
+
+  /*****************************************************************************
+   *
+   * Event listeners for UI elements
+   *
+   ****************************************************************************/
+
+  // document.getElementById('butCheckout').addEventListener('click', function() {
+  //   // Refresh all of the forecasts
+  //   app.updateForecasts();
+  // });
+
+
+  /*****************************************************************************
+   *
+   * Methods to update/refresh the UI
+   *
+   ****************************************************************************/
+
+})();
